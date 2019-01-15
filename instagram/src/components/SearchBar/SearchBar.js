14a8eb47af539afd4e917c8067f6840860fb12a5
Angel Torres <alas4224@gmail.com>
2019-01-14 18:10:29 -0600

    added more styles to search header

--- instagram/src/components/SearchBar/SearchBar.js
+++ instagram/src/components/SearchBar/SearchBar.js
@@ -5,14 +5,14 @@ const SearchBar = (props) => {
     return (
       <div className="search-bar">
         <div className="left-search-container">
-            <img src="https://img.icons8.com/ios/30/000000/instagram-new.png"/>
+            <img className="insta-logo" src="https://img.icons8.com/ios/30/000000/instagram-new.png"/>
             <h1>Instagram</h1>
         </div>
         <input type="text" placeholder="search"/>
         <div className="right-search-container">
-            <img src="https://img.icons8.com/ios/50/000000/compass.png"/>
-            <img src="https://img.icons8.com/ios/50/000000/like.png" />
-            <img src="https://img.icons8.com/ios/50/000000/gender-neutral-user.png"/>
+            <img className="search-logo" src="https://img.icons8.com/ios/50/000000/compass.png"/>
+            <img className="search-logo" src="https://img.icons8.com/ios/50/000000/like.png" />
+            <img className="search-logo" src="https://img.icons8.com/ios/50/000000/gender-neutral-user.png"/>
         </div>
       </div>
     )
