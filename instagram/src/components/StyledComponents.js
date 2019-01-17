1471b3931f02dc24a7dc677fd88861e12aa7d119
Angel Torres <alas4224@gmail.com>
2019-01-17 16:12:46 -0600

    u

--- instagram/src/components/StyledComponents.js
+++ instagram/src/components/StyledComponents.js
@@ -2,7 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 
 
-const styledButton = styled.button`
+const StyledButton = styled.button`
     padding: 10px;
     width: 100px;
     margin: 20px;
@@ -14,14 +14,14 @@ const styledButton = styled.button`
     }
 `;
 
-const logInInput = styled.input`
+const LogInInput = styled.input`
     margin: 20px;
     width: 200px;
     height: 30px;
     text-align: center;
 `;
 
-const logInForm = styled.form`
+const LogInForm = styled.form`
     border: 2px solid lightgrey;
     width: 500px;
     margin: auto auto;
@@ -32,4 +32,8 @@ const logInForm = styled.form`
     align-items: center;
 `;
 
-export default {styledButton, logInInput, logInForm};
+module.exports = {
+    StyledButton: StyledButton,
+    LogInInput: LogInInput,
+    LogInForm: LogInForm,
+}
